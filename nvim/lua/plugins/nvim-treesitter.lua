dc0263ceb3520e1b3c863a16e12b5d62232bf7ba
Graham Schellenberg <GSchellenberg@cubresa.com>
2024-12-30 20:36:48 -0600

    Add treesitter-context plugin

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -8,4 +8,23 @@ return {
       },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    keys = {
+      {
+        "[x",
+        function()
+          require("treesitter-context").go_to_context(vim.v.count1)
+        end,
+        { silent = true },
+        desc = "Jump up to context (treesitter-context)",
+      },
+      {
+        "<leader>ut",
+        "<CMD>TSContextToggle<CR>",
+        desc = "Toggle context (treesitter-context)",
+      },
+    },
+  },
 }
