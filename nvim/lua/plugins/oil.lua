fb11ac707ded0a00069a538d2b491730f7b27c59
Graham Schellenberg <GSchellenberg@cubresa.com>
2025-05-01 16:20:02 +0000

    Update oil convience

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -8,13 +8,17 @@ return {
       view_options = {
         show_hidden = true,
       },
+      keymaps = {
+        ["<BS>"] = { "actions.parent", mode = "n" },
+        ["H"] = { "actions.toggle_hidden", mode = "n" },
+      },
     },
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
     keys = {
       {
-        "<leader>oi",
+        "<leader>o",
         "<cmd>Oil<cr>",
         desc = "oil (file management)",
       },
